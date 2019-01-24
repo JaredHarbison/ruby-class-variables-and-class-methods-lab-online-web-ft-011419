57f9105d234f2990082e993ecb2ab9bcff93f3e4
Jared Harbison <jared.harbison@gmail.com>
2019-01-24 17:41:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ attr_accessor :name, :artist, :genre
 
   @@count = 0 
   @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -10,6 +11,7 @@ attr_accessor :name, :artist, :genre
     @genre = genre 
     @@count += 1
     @@artists << artist 
+    @@genres << genre 
   end
   
   def self.count 
