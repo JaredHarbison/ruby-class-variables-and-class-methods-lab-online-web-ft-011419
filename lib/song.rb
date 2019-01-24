8b74aa667fba96f71ad35159dc299c3b6c891a79
Jared Harbison <jared.harbison@gmail.com>
2019-01-24 17:33:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+attr_accessor :name, :artist, :genre   
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+  end
+  
+  @@count = 0 
+  def self.count
+    @@count
+  end
+  
+end 
