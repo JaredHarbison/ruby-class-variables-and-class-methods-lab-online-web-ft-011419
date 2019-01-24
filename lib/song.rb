1094e2efc1ab77285974961a5bc244f0f28990b2
Jared Harbison <jared.harbison@gmail.com>
2019-01-24 17:57:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,6 +21,10 @@ attr_accessor :name, :artist, :genre
   def self.artists 
     @@artists.uniq
   end 
+    
+  def self.genres
+    @@genres.uniq 
+  end 
   
   def self.artist_count
     artist_count = {}
@@ -35,11 +39,14 @@ attr_accessor :name, :artist, :genre
   end
   
   def self.genre_count
-    @@genre_count = {}
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else genre_count[genre] = 1
+    end
   end 
-  
-  def self.genres
-    @@genres.uniq 
-  end 
-  
+    genre_count
+  end
+
 end 
